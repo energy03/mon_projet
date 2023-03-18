d932207cbce411ca7938322765bf99f1c7dec59a
“ian.ngoune” <ian.ngoune@gmail.com>
2023-03-18 12:29:11 +0100

    Tâche A.1: Creation du fichier main.c et importation des bibliothèque et interfaces

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "joueuses.h"
+#include "personnages.h"
+#include "zones.h"
+#include "cartes.h"
+#include "interface.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
