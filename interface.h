454dec78b26bea964ef78da3ff34e569d1a6d8ad
Pape Alassane NDIAYE <papealassane.ndiaye@pc251-08.pedago.ensiie.fr>
2023-03-20 15:10:15 +0100

    Tache A4 : modification de fonctions interface

--- interface.h
+++ interface.h
@@ -7,18 +7,19 @@
 /*
 Fonction pour afficher toutes les informations du jeu
 */
-void afficher_informations();
+void afficher_informations(personnage* monstre, joueuse* j1, joueuse* j2); //-----
 
 /*
 Fonction pour demander à une joueuse combien de capital elle veut utiliser  
 */
-int demander_capital_joueuse(joueuse* joueeuse);
+int demander_capital_joueuse(joueuse* joueuse); //-------
 
 /*
 Fonction pour demander à une joueuse si elle veut jouer une carte et si oui, laquelle
 Si elle ne désire pas jouer une carte, on retourne NULL
+on affichera le joueur a qui c'est le tour
 */
-carte* demander_carte_joueuse(joueuse* joueeuse);
+carte* demander_carte_joueuse(joueuse* joueuse);
 
 /*
 Fonction pour afficher un message quand le jeu est fini
@@ -28,12 +29,12 @@ void afficher_fin_jeu();
 /*
 Fonction pour afficher si un membre a été mangé
 */
-void afficher_membre_mangE();
+void afficher_membre_mangE(personnage* personnage);
 
 /*
 Fonction pour afficher si un membre a été déplacé
 */
-void afficher_membre_deplacE();
+void afficher_membre_deplacE(personnage* personnage);
 
 
 #endif
