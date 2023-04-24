e187e22ceee357fa263000edef2b0081ac054d0f
Pape Alassane NDIAYE <papealassane.ndiaye@ensiie.fr>
2023-04-24 21:07:31 +0200

    Tache A4 : Rajout d'une fonction demanderZone

--- headers/interface.h
+++ headers/interface.h
@@ -14,6 +14,13 @@ Fonction pour demander à une joueuse combien de capital elle veut utiliser
 */
 int demander_capital_joueuse(joueuse* joueuse, int tour_joueuse); //-------
 
+
+/*
+Fonction pour demander à une joueuse sur quelle zone elle veut jouer
+*/
+int demander_zone_joueuse(joueuse* joueuse, int tour_joueuse) 
+
+
 /*
 Fonction pour demander à une joueuse si elle veut jouer une carte et si oui, laquelle
 Si elle ne désire pas jouer une carte, on retourne NULL
