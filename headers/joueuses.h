37a8e826ec5da41a4cf9e2a794844258701b2dad
Donatien <donatienkao2003@gmail.com>
2023-04-24 16:08:30 +0200

    Taches B5 (provisore 2): types, getters , setters

--- headers/joueuses.h
+++ headers/joueuses.h
@@ -49,4 +49,78 @@ void utiliser_capital(joueuse* joueuse, int montant);
 // Fonction pour permettre à une joueuse de jouer une carte
 void jouer_carte(joueuse* joueuse, carte* carte);
 
+/*****************************************
+ * ***************************************
+ * ***************************************
+ *       TACHE B5
+ * ***************************************
+ * ***************************************
+ * ***************************************
+*/
+
+/// @brief recupération du nom d'une joueuse  
+/// @param player pointeur sur la joueuse
+/// @return nom de la joueuse
+char* get_nom_joueuse(joueuse player);
+
+/// @brief recupération du capital d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @return capital de la joueuse
+int get_capital_joueuse(joueuse player);
+
+/// @brief recupération du nombre de membres d'une joueuse
+
+
+/// getters
+/// @brief recupération du nom d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @return nombre de membres de la joueuse
+char* get_nom_joueuse(joueuse player);
+
+/// @brief recupération du nombre de cartes d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @return nombre de cartes de la joueuse
+int get_nb_cartes_joueuse(joueuse player);
+
+/// @brief recupération du nombre de membres d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @return nombre de membres de la joueuse
+int get_nb_membres_joueuse(joueuse player);
+
+/// @brief recupération du nombre de cartes d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @return nombre de cartes de la joueuse
+int get_nb_cartes_joueuse(joueuse player);
+
+// setters
+
+/// @brief modification du nom d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @param nom nouveau nom de la joueuse
+void set_nom_joueuse(joueuse player, char* nom);
+
+/// @brief modification du capital d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @param capital nouveau capital de la joueuse
+void set_capital_joueuse(joueuse player, int capital);
+
+/// @brief modification du nombre de membres d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @param nb_membres nouveau nombre de membres de la joueuse
+void set_nb_membres_joueuse(joueuse player, int nb_membres);
+
+/// @brief modification du nombre de cartes d'une joueuse
+/// @param player pointeur sur la joueuse
+/// @param nb_cartes nouveau nombre de cartes de la joueuse
+void set_nb_cartes_joueuse(joueuse player, int nb_cartes);
+
+/*****************************************
+ * ***************************************
+ * ***************************************
+ *       TACHE B5
+ * ***************************************
+ * ***************************************
+ * ***************************************
+*/
+
 #endif
