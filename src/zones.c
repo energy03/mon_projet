193aa098ee9e2edac94a89998f026cbd1a79b6b8
Pape Alassane NDIAYE <papealassane.ndiaye@ensiie.fr>
2023-04-24 21:10:55 +0200

    Tache B6 : implémentation zones.c

--- src/zones.c
+++ src/zones.c
@@ -1,6 +1,7 @@
 #include "../headers/zones.h"
 #include "../headers/personnages.h"
-
+#include <stdio.h>
+#include <time.h>
 
 /*****************************************
  * ***************************************
@@ -81,3 +82,81 @@ void set_nb_zones(zones* zones, int nb_zones)
  * ***************************************
 */
 
+
+// Fonction pour créer une zone
+zones* createZones(int nb_zones){
+        double probabilities[10][10];
+        zones* zones_jeu=malloc(sizeof(zones));
+        zones_jeu->nb_zones=nb_zones;
+        for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                        zones_jeu->probabilities[i][j] = probabilities[i][j];
+                }
+        }
+        zone* zone1 = malloc(sizeof(zone));
+        zone1->num_zone=1;
+        zone1->nb_monster=1;
+        zone1->nb_perso=0;
+        zones_jeu->tab_zone[0]=zone1;
+        zone* zone2 = malloc(sizeof(zone));
+        zone2->num_zone=2;
+        zone2->nb_monster=0;
+        zone2->nb_perso=0;
+        zones_jeu->tab_zone[1]=zone2;
+        zone* zone3 = malloc(sizeof(zone));
+        zone3->num_zone=3;
+        zone3->nb_perso=0;
+        zone3->nb_monster=0;
+        zones_jeu->tab_zone[2]=zone3;
+        zone* zone4 = malloc(sizeof(zone));
+        zone4->num_zone=4;
+        zone4->nb_monster=0;
+        zone4->nb_perso=0;
+        zones_jeu->tab_zone[3]=zone4;
+        zone* zone5 = malloc(sizeof(zone));
+        zone5->num_zone=5;
+        zone5->nb_monster=0;
+        zone5->nb_perso=0;
+        zones_jeu->tab_zone[4]=zone5;
+        zone* zone6 = malloc(sizeof(zone));
+        zone6->num_zone=6;
+        zone6->nb_monster=0;
+        zone6->nb_perso=0;
+        zones_jeu->tab_zone[5]=zone6;
+        zone* zone7 = malloc(sizeof(zone));
+        zone7->num_zone=7;
+        zone7->nb_monster=0;
+        zone7->nb_perso=0;
+        zones_jeu->tab_zone[6]=zone7;
+        zone* zone8 = malloc(sizeof(zone));
+        zone8->num_zone=8;
+        zone8->nb_monster=0;
+        zone8->nb_perso=0;
+        zones_jeu->tab_zone[7]=zone8;
+        zone* zone9 = malloc(sizeof(zone));
+        zone9->num_zone=9;
+        zone9->nb_monster=0;
+        zone9->nb_perso=0;
+        zones_jeu->tab_zone[8]=zone9;
+        zone* zone10 = malloc(sizeof(zone));
+        zone10->num_zone=10;
+        zone10->nb_monster=0;
+        zone10->nb_perso=0;
+        zones_jeu->tab_zone[9]=zone10;
+        return zones_jeu;
+}
+
+// Fonction pour libérer la mémoire allouée lors de la création des zones
+void freeZones(zones* zones) {
+    free(zones);
+}
+
+// Fonction pour récupérer une zone à partir de son numéro
+zone* getZoneById(zones* zones, int numZone) {
+    if (numZone >= 0 && numZone < zones->nb_zones) {
+        return &zones->tabZones[numZone+1];
+    } else {
+        printf("Erreur: Numéro de zone invalide.\n");
+        exit(EXIT_FAILURE);
+    }
+}
